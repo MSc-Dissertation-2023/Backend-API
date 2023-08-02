dfd3d4a772e1d34effab0f43394557eaa13153aa
jamesrcraigie <jamesrcraigie1@gmail.com>
2023-08-02 13:57:42 +0100

    Created endpoints for DB

---
+++ app/controllers/leaderboards_controller.rb
@@ -0,0 +1,13 @@
+class LeaderboardsController < ApplicationController
+    def index
+        records = Leaderboard.all
+        records = records.order(score: :desc).limit(10)
+        render json: records
+    end
+
+    def create
+        params['name']
+        params['score']
+        Leaderboard.create(name: params['name'], score: params['score'])
+    end
+end
