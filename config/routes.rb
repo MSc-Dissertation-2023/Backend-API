5ce3973b2c542b76b90d3d68e1a6a7af53082dba
Sheng <cyusheng93@gmail.com>
2023-08-12 13:39:50 +0100

    Added a root welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "request_guest_token", to
   root "welcome#index"
-  resources :guest_tokens
-  resources :leaderboards
-  resources :stage_one_stats
+  resources :guest_tokens, only: [:create]
+  resources :leaderboards, only: [:index, :create]
+  resources :stage_one_stats, only: [:index, :create]
   resources :stage_two_stats, only: [:index, :create]
   resources :stage_two_dda, only: [:index, :create]
   resources :stage_two_enemy_metrics, only: [:index, :create]
